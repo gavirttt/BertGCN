39a7bd664e82f4b64276e387dcea34f786789490
gavirttt <glvirtucio@up.edu.ph>
2026-03-20 16:24:03 +0800

    fix: sorted label_set to be consistent with mapping in train_bert_gcn.py

--- build_graph.py
+++ build_graph.py
@@ -243,8 +243,9 @@ Word definitions end
 label_set = set()
 for doc_meta in shuffle_doc_name_list:
     temp = doc_meta.split('\t')
-    label_set.add(temp[2])
-label_list = list(label_set)
+    if temp[2] != 'unlabeled':        # ← exclude unlabeled
+        label_set.add(temp[2])
+label_list = sorted(list(label_set))
 
 label_list_str = '\n'.join(label_list)
 # Fix: Add encoding='utf-8' to file writes
